52f3d661c129deea9e4cec36479a7fe0a590dd52
tordsyabe <46318298+tordsyabe@users.noreply.github.com>
2021-07-06 22:07:59 +0400

    added heroku for deployment

--- amazon.py
+++ amazon.py
@@ -1,17 +1,20 @@
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 import datetime
+import os
 
 
 def amazon_crawler(url):
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
     price_symbol = ""
     webpage = "www.amazon.ae"
     data = []
@@ -26,8 +29,6 @@ def amazon_crawler(url):
 
     date_today = datetime.datetime.now()
     timestamp = date_today.strftime("%Y-%m-%d %H:%M:%S")
-
-    driver = webdriver.Chrome('chromedriver')
 
     split_url = url.split("/")
 
