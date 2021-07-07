f48df2a3ec81a393bec0effd0d31623a95953427
tordsyabe <46318298+tordsyabe@users.noreply.github.com>
2021-07-07 23:03:13 +0400

    added window size to 1400 x900

--- noon.py
+++ noon.py
@@ -11,6 +11,7 @@ def noon_crawler(url):
     print("entered noon_crawler")
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("window-size=1400,900")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -50,8 +51,8 @@ def noon_crawler(url):
         print("failed to open link")
         pass
     try:
-        element = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.XPATH, '//div[@class="productContainer"]'))
+        element = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//div[@class="productContainer"]'))
         )
 
         element.click()
