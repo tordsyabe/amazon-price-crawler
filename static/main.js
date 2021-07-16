8295eab8a8b80c13b85a944009c908915c8ec155
tordsyabe <46318298+tordsyabe@users.noreply.github.com>
2021-07-16 12:18:12 +0400

    added copy to clipboard for emails

--- static/main.js
+++ static/main.js
@@ -20,4 +20,32 @@ $(document).ready(function() {
 
 $(document).ready(function () {
       $('#emailsTable').DataTable();
-      });
+});
+
+$(document).ready(function(){
+
+    var textAreaForEmails = $('.emailAdds');
+    var emailAddresses = ""
+
+    $('#emailsTable td:first-child').each(function(){
+        emailAddresses += $(this).text() + ";";
+    });
+
+    textAreaForEmails.val(emailAddresses);
+
+    $('#copyEmailAddresses').on('click', function(){
+        $("#copyAlert").show();
+
+        textAreaForEmails.select();
+        document.execCommand('copy');
+
+    });
+
+    $('#closeCopyAlert').on('click',function(e){
+        e.preventDefault();
+        $("#copyAlert").hide();
+    });
+
+
+
+});
